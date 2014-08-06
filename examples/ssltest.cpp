627ad1041fa90c81935676738715793b824444d4
netmind <netmind@netlinux>
2014-08-06 14:44:30 +0900

    add ssl sample

--- examples/ssltest.cpp
+++ examples/ssltest.cpp
@@ -10,20 +10,18 @@
 #define DBG_LEVEL DBG_DEBUG
 
 #include "edslog.h"
+#include "edssl/EdSSL.h"
 #include "EdNio.h"
 #include "EdTask.h"
-#include "EsFile.h"
 #include "edssl/EdSSLSocket.h"
 
 using namespace std;
 using namespace edft;
 
-
-
 class SslTask: public EdTask, public EdSSLSocket::ISSLSocketCb
 {
 	EdSSLSocket *mssl;
-	EsFile wfile;
+	SSL_CTX *mSSLCtx;
 
 	virtual int OnEventProc(EdMsg* pmsg)
 	{
@@ -31,21 +29,23 @@ class SslTask: public EdTask, public EdSSLSocket::ISSLSocketCb
 		{
 			dbgd("edm init.......");
 
-			wfile.openFile("body.txt", EsFile::OPEN_RWTC);
 
 			mssl = new EdSSLSocket();
+			mSSLCtx = EdSSL::buildClientCtx(SSL_VER_TLSV1);
+			mssl->openSSLClientSock(mSSLCtx);
 			mssl->setSSLCallback(this);
-			mssl->sslConnect("127.0.0.1");
-			//mssl->sslConnect("173.194.72.94");
+			mssl->connect("127.0.0.1", 8000);
 		}
-		else if(pmsg->msgid == EDM_CLOSE)
+		else if (pmsg->msgid == EDM_CLOSE)
 		{
 			dbgd("edm close.......");
-			if(mssl)
+			if (mssl)
 			{
-				mssl->sslClose();
+				mssl->close();
 				delete mssl;
 			}
+
+			EdSSL::freeCtx(mSSLCtx);
 		}
 		return 0;
 	}
@@ -55,15 +55,20 @@ class SslTask: public EdTask, public EdSSLSocket::ISSLSocketCb
 		if (event == SSL_EVENT_CONNECTED)
 		{
 			dbgd("ssl connected...");
-			char msg[] = "GET /test.dat HTTP/1.1\r\n"
-					"User-Agent: curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3\r\n"
-					"Host: 127.0.0.1\r\n"
-					//"Host: github.com\r\n"
-					//"Host: www.google.co.kr\r\n"
-					"Accept: */*\r\n"
-					"\r\n";
-			int wret = mssl->sslSend(msg, strlen(msg));
-			dbgd(" send request, ret=%d", wret);
+			check_cert(psock->getSSL(), "netsvr");
+			if(1)
+			{
+				char msg[] = "GET /test.dat HTTP/1.1\r\n"
+						"User-Agent: curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3\r\n"
+						"Host: 127.0.0.1\r\n"
+						//"Host: github.com\r\n"
+						//"Host: www.google.co.kr\r\n"
+						"Accept: */*\r\n"
+						"\r\n";
+				int wret = mssl->send(msg, strlen(msg));
+				dbgd(" send request, ret=%d", wret);
+			}
+
 
 		}
 		else if (event == SSL_EVENT_DISCONNECTED)
@@ -73,21 +78,45 @@ class SslTask: public EdTask, public EdSSLSocket::ISSLSocketCb
 			delete mssl;
 			mssl = NULL;
 		}
-		else if(event == SSL_EVENT_READ)
+		else if (event == SSL_EVENT_READ)
 		{
-			static int recvcnt=0;
-			char buf[256+1];
-			int rdcnt = mssl->sslRecv(buf, 256);
-			//dbgd("rdcnt=%d",rdcnt);
-			if(rdcnt>0) {
-				wfile.writeFile(buf, rdcnt);
+			static int recvcnt = 0;
+			char buf[256 + 1];
+			int rdcnt = mssl->recv(buf, 256);
+
+			if (rdcnt > 0)
+			{
 				recvcnt += rdcnt;
 			}
 
-			if(recvcnt >= 159350056-203840)
+			if (recvcnt >= 159350056 - 203840)
 				dbgd("rdcnt=%d", rdcnt);
 
 		}
+	}
+
+	void check_cert(SSL *ssl, char *host)
+	{
+		X509 *peer;
+		char peer_CN[256];
+		if (SSL_get_verify_result(ssl) != X509_V_OK)
+			printf("### Certificate doesn’t verify\n");
+
+		/*Check the cert chain. The chain length
+
+		 is automatically checked by OpenSSL when
+
+		 we set the verify depth in the ctx */
+
+		/*Check the common name*/
+
+		peer = SSL_get_peer_certificate(ssl);
+		X509_NAME_get_text_by_NID(X509_get_subject_name(peer), NID_commonName, peer_CN, 256);
+
+		if (strcasecmp(peer_CN, host))
+			printf("### Common name doesn’t match host name, peer=%s\n", peer_CN);
+		X509_free(peer);
+
 	}
 };
 
@@ -98,6 +127,7 @@ void testsslclient()
 	getchar();
 	task.terminate();
 }
+
 
 int main()
 {
