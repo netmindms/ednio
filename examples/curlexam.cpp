f37ecc97fed128b2f906cf56fd2ab73f4f1f86e3
blake kim <netmindms@gmail.com>
2014-08-11 01:06:24 +0900

    modify curl example.

--- examples/curlexam.cpp
+++ examples/curlexam.cpp
@@ -1,11 +1,13 @@
 //============================================================================
 // Name        : curlexam.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#define DBGTAG "appcl"
+#define DBG_LEVEL DBG_DEBUG
 
 #include "edslog.h"
 #include "EdNio.h"
@@ -14,32 +16,55 @@
 #include "edssl/EdSSL.h"
 #include "edssl/EdSSLSocket.h"
 #include "edcurl/EdCurl.h"
+#include "edcurl/EdMultiCurl.h"
 
 using namespace std;
 using namespace edft;
 
-class MainTask: public EdTask
+class MainTask: public EdTask, public EdCurl::ICurlStatusCb
 {
-	EdCurl *ecurl;
+	EdMultiCurl* multi;
+	EdCurl *edcurl;
 	virtual int OnEventProc(EdMsg* pmsg)
 	{
 		if (pmsg->msgid == EDM_INIT)
 		{
-
-			ecurl = new EdCurl();
-
-			ecurl->open();
-			ecurl->request("http://127.0.0.1");
+			multi = new EdMultiCurl;
+			dbgd("new multi=%p", multi);
+			multi->open();
+			edcurl = new EdCurl();
+			dbgd("new alloc curl=%p", edcurl);
+			edcurl->setCallback(this);
+			edcurl->open(multi);
+			//edcurl->request("http://www.google.com");
+			//edcurl->request("http://www.naver.com");
+			edcurl->request("http://www.yahoo.com");
+			//edcurl->request("http://127.0.0.1");
+			//setTimer(1, 1, 1);
 
 		}
 		else if (pmsg->msgid == EDM_CLOSE)
 		{
-			if(ecurl) {
-				ecurl->closeCurl();
+			if(edcurl) {
+				edcurl->close();
 			}
+			delete edcurl;
+			delete multi;
 		}
-
+		else if(pmsg->msgid == EDM_TIMER)
+		{
+			dbgd("cancel timer.......");
+			killTimer(1);
+			edcurl->close();
+		}
 		return 0;
+	}
+
+	virtual void IOnCurlStatus(EdCurl* pcurl, int status)
+	{
+		dbgd("curl result, code=%d", status);
+		pcurl->close();
+		delete pcurl;
 	}
 
 };
